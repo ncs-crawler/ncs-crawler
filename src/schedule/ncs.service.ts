d00084edba0b47a5a5b0f3658a0785714e795a36
shilinzhu <shilinzhu@tencent.com>
2019-05-16 22:51:42 +0800

    feat: add docker image

--- src/schedule/ncs.service.ts
+++ src/schedule/ncs.service.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { Cron, Interval, Timeout, NestSchedule } from '@nestcloud/schedule';
 import Browser from '../common/browser';
 
+const URL = 'https://www.youtube.com/user/NoCopyrightSounds/playlists';
 @Injectable()
 export class NCSService extends NestSchedule {
   // 每小时执行一次
   // @Cron('* * */1 * * *')
   @Timeout(2000)
   async crawl() {
-    // console.log('executing cron job');
-    const browser = new Browser();
-    const page = await browser.newPage();
-    await page.goto('https://www.youtube.com/user/NoCopyrightSounds/playlists');
-    await browser.close();
+    console.log('executing cron job');
+    // const browser = new Browser();
+    // const page = await browser.newPage();
+    // await page.goto(URL);
+    // await browser.close();
   }
 }
